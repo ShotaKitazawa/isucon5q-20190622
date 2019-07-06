29f5fafbce547f9793e12e9a31c55f3f25f0a100
ShotaKitazawa <skitazawa1121@gmail.com>
2019-07-06 19:20:33 +0900

    update

--- app.go
+++ app.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"encoding/hex"
 	"errors"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -32,6 +33,97 @@ var (
 	userByAccountName map[string]User
 	userAuth          map[string]UserAuth
 	mu                sync.Mutex
+)
+
+const (
+	helloworld = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <title>ISUxi</title>
+</head>
+<body class="container">
+<h1 class="jumbotron"><a href="/">ISUxiへようこそ!</a></h1>
+<h2>ISUxi login</h2>
+<div class="text-danger" id="logout-message">高負荷に耐えられるSNSコミュニティサイトへようこそ!</div>
+<div id="login-form">
+  <form method="POST" action="/login">
+    <div class="col-md-4 input-group">
+      <span class="input-group-addon">E-mail</span>
+      <input class="form-control" type="text" name="email" placeholder="E-mail address" />
+    </div>
+    <div class="col-md-4 input-group">
+      <span class="input-group-addon">パスワード</span>
+      <input class="form-control" type="password" name="password" />
+    </div>
+    <div class="col-md-1 input-group">
+      <input class="btn btn-default" type="submit" name="Login" value="Login" />
+    </div>
+  </form>
+</div>
+</body>
+</html>`
+	loginFailed = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <title>ISUxi</title>
+</head>
+<body class="container">
+<h1 class="jumbotron"><a href="/">ISUxiへようこそ!</a></h1>
+<h2>ISUxi login</h2>
+<div class="text-danger" id="logout-message">ログインに失敗しました</div>
+<div id="login-form">
+  <form method="POST" action="/login">
+    <div class="col-md-4 input-group">
+      <span class="input-group-addon">E-mail</span>
+      <input class="form-control" type="text" name="email" placeholder="E-mail address" />
+    </div>
+    <div class="col-md-4 input-group">
+      <span class="input-group-addon">パスワード</span>
+      <input class="form-control" type="password" name="password" />
+    </div>
+    <div class="col-md-1 input-group">
+      <input class="btn btn-default" type="submit" name="Login" value="Login" />
+    </div>
+  </form>
+</div>
+</body>
+</html>`
+	onlyFriendAccess = `
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <title>ISUxi</title>
+</head>
+<body class="container">
+<h1 class="jumbotron"><a href="/">ISUxiへようこそ!</a></h1>
+<h2>エラー</h2>
+<div class="text-danger">友人のみしかアクセスできません</div>
+<div><a href="/">戻る</a></div>
+</body>
+</html>`
+	contentNotFound = `
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <title>ISUxi</title>
+</head>
+<body class="container">
+<h1 class="jumbotron"><a href="/">ISUxiへようこそ!</a></h1>
+<h2>エラー</h2>
+<div class="text-danger">要求されたコンテンツは存在しません</div>
+<div><a href="/">戻る</a></div>
+</body>
+</html>`
 )
 
 type User struct {
@@ -235,13 +327,13 @@ func myHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
 					session := getSession(w, r)
 					delete(session.Values, "user_id")
 					session.Save(r, w)
-					render(w, r, http.StatusUnauthorized, "login.html", struct{ Message string }{"ログインに失敗しました"})
+					fmt.Fprintf(w, loginFailed)
 					return
 				case rcv == ErrPermissionDenied:
-					render(w, r, http.StatusForbidden, "error.html", struct{ Message string }{"友人のみしかアクセスできません"})
+					fmt.Fprintf(w, onlyFriendAccess)
 					return
 				case rcv == ErrContentNotFound:
-					render(w, r, http.StatusNotFound, "error.html", struct{ Message string }{"要求されたコンテンツは存在しません"})
+					fmt.Fprintf(w, contentNotFound)
 					return
 				default:
 					var msg string
@@ -334,7 +426,8 @@ func render(w http.ResponseWriter, r *http.Request, status int, file string, dat
 }
 
 func GetLogin(w http.ResponseWriter, r *http.Request) {
-	render(w, r, http.StatusOK, "login.html", struct{ Message string }{"高負荷に耐えられるSNSコミュニティサイトへようこそ!"})
+	fmt.Fprintf(w, helloworld)
+
 }
 
 func PostLogin(w http.ResponseWriter, r *http.Request) {
